970fe1b83749e348fb91b28e2e0d9b5d34dc9937
HungryPants <yelamanvalikhanovich@gmail.com>
2018-11-18 10:07:48 +0600

    User_account_added

--- app/src/main/java/com/example/yelaman/alphaacademy/UserFragment.java
+++ app/src/main/java/com/example/yelaman/alphaacademy/UserFragment.java
@@ -2,7 +2,10 @@ package com.example.yelaman.alphaacademy;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +16,8 @@ import android.view.ViewGroup;
  */
 public class UserFragment extends Fragment {
 
-
-    public UserFragment() {
-        // Required empty public constructor
-    }
-
+    private CardView accountinformation;
+    private CardView settings;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,4 +26,20 @@ public class UserFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        accountinformation = view.findViewById(R.id.cardAccount);
+
+        settings = view.findViewById(R.id.settings);
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
 }
